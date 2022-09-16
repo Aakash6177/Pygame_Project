3f029bfbf13e034f974e6dd671159830d9471da2
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2022-09-16 09:58:22 -0700

    animated the water and cloud

--- abc.py
+++ abc.py
@@ -7,9 +7,12 @@ land_bg = pygame.image.load('./shooting range assets/Land_BG.png')
 water_bg = pygame.image.load('./shooting range assets/Water_BG.png')
 cloud_1 = pygame.image.load('./shooting range assets/Cloud1.png')
 cloud_2 = pygame.image.load('./shooting range assets/Cloud2.png')
-land_position_y = 560
+land_position_y = 540
 land_speed = 1
-
+water_position_y = 640
+water_speed = 2
+cloud_1_position_x = 200
+cloud_1_speed = 0.5
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 while True:
@@ -17,12 +20,20 @@ while True:
         if (event.type == pygame.QUIT): # this allows pygame to listen for all the events happening on the window
             pygame.quit() # this will listen for the event for you to quit the window
             sys.exit()
-    land_position_y -= 1
+    
+    water_position_y = water_position_y - water_speed
+    # cloud_1_position_x = cloud_1_position_x + cloud_1_speed
+    if (water_position_y < 530 or water_position_y > 650):
+        water_speed = water_speed * -1
+        
+    if (cloud_1_position_x < 400):
+        cloud_1_position_x = cloud_1_position_x + cloud_1_speed
+
 
     screen.blit(wood_bg,(0,0))
     screen.blit(land_bg,(0,land_position_y))
-    screen.blit(water_bg,(0,640))
-    screen.blit(cloud_1, (200,0))
+    screen.blit(water_bg,(0,water_position_y))
+    screen.blit(cloud_1, (cloud_1_position_x,0))
     screen.blit(cloud_2,(700,100))
     screen.blit(cloud_1, (1000,50))
     screen.blit(cloud_2,(50,100))
